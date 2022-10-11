17b565bdc94e22bafd083fb2ffc64cc68b5b6ec2
Jorge Sanmiguel <sanmiguelgimeno@hotmail.com>
2022-10-11 22:03:46 +0200

    feat: Add debounce time to InputSignal

--- main/src/Utils/InterfaceUtils.h
+++ main/src/Utils/InterfaceUtils.h
@@ -7,19 +7,39 @@
 #include <Arduino.h>
 
 #define ANALOG_PIN_HIGH_THRESHOLD 800 // TODO CHECK VALUE
+#define DEBOUNCE_TIME_MILLIS 200
 
 class InputSignal
 {
     public:
         InputSignal(const uint8_t pinRef) : pinRef(pinRef) {
             setPinMode();
+
+            lastUpdateTime = millis();
+            _value         = analogRead(pinRef) >= ANALOG_PIN_HIGH_THRESHOLD;
         }
 
-        boolean value() {
-            return analogRead(pinRef) >= ANALOG_PIN_HIGH_THRESHOLD;
+        bool value() {
+            uint32_t time = millis();
+
+            bool read_value = analogRead(pinRef) >= ANALOG_PIN_HIGH_THRESHOLD;
+
+            if (read_value == _value) {
+                lastUpdateTime = time;
+            }
+            else if (time - lastUpdateTime >= DEBOUNCE_TIME_MILLIS) {
+                lastUpdateTime = time;
+                _value         = read_value;
+            }
+
+            return _value;
         }
+
     private:
         const uint8_t pinRef;
+
+        uint32_t lastUpdateTime;
+        bool     _value;
 
         void setPinMode() {
             pinMode(pinRef, INPUT);
